9dc65c9611e9f386d8a587aa683445db3af0f12c
sanusaurabh <san.619jaan@gmail.com>
2017-10-02 23:31:19 +0530

    change table name for role

--- demo/src/main/java/com/example/demo/auth/model/Role.java
+++ demo/src/main/java/com/example/demo/auth/model/Role.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "appuser")
+@Table(name = "approle")
 public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
